f2443530ca77abcc079003a7761bf5209a2f1740
richardkocian <richardkocian4@gmail.com>
2022-04-01 21:58:32 +0200

    Přidána tolerance u testů

--- src/tests/TestMathLib.java
+++ src/tests/TestMathLib.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class TestMathLib {
 
     private MathLib mathLib;
+    private double delta = 0.0000000000001;
 
     @Before
     public void setUp() {
@@ -19,15 +20,15 @@ public class TestMathLib {
         assertEquals(60,mathLib.plus(30,30),0);
         assertEquals(77,mathLib.plus(29,48),0);
         assertEquals(-62990,mathLib.plus(1597,-64587),0);
-        assertEquals(-33397.5709,mathLib.plus(923.8291,-34321.4),0);
-        assertEquals(421.5865,mathLib.plus(-46.5481,468.1346),0);
+        assertEquals(-33397.5709,mathLib.plus(923.8291,-34321.4),delta);
+        assertEquals(421.5865,mathLib.plus(-46.5481,468.1346),delta);
     }
 
     @Test
     public void minusTest() {
         assertEquals(0,mathLib.minus(30,30),0);
         assertEquals(-31,mathLib.minus(43,74),0);
-        assertEquals(-4619.054,mathLib.minus(58.946,4678),0);
+        assertEquals(-4619.054,mathLib.minus(58.946,4678),delta);
     }
 
     @Test
@@ -35,27 +36,27 @@ public class TestMathLib {
         assertEquals(15,mathLib.mul(3,5),0);
         assertEquals(45,mathLib.mul(3,15),0);
         assertEquals(-448,mathLib.mul(16,-28),0);
-        assertEquals(35614426.3776875,mathLib.mul(4628.6375, 7694.365),0);
+        assertEquals(35614426.3776875,mathLib.mul(4628.6375, 7694.365),delta);
     }
 
     @Test
     public void divTest() {
         assertEquals(16,mathLib.div(64,4),0);
-        assertEquals(0.257600025576631,mathLib.div(1658.365,6437.75169),0);
-        assertEquals(4.114015916158117,mathLib.div(-264.73569,-64.3497),0);
+        assertEquals(0.257600025576631,mathLib.div(1658.365,6437.75169),delta);
+        assertEquals(4.114015916158117,mathLib.div(-264.73569,-64.3497),delta);
     }
 
     @Test
     public void modTest() {
         assertEquals(0,mathLib.mod(20,5),0);
-        assertEquals(11.1433,mathLib.mod(281.3891,12.2839),0);
-        assertEquals(-360.7452627,mathLib.mod(-2329.294132,384.2913421),0);
+        assertEquals(11.140000000000025,mathLib.mod(281.3,12.28),delta);
+        assertEquals(-23.54607939999994,mathLib.mod(-2329.294132,384.2913421),delta);
     }
 
     @Test
     public void sqrtTest() {
         assertEquals(4,mathLib.sqrt(16),0);
-        assertEquals(25.442424609301685,mathLib.sqrt(647.31697),0);
+        assertEquals(25.442424609301685,mathLib.sqrt(647.31697),delta);
         assertEquals(0,mathLib.sqrt(0),0);
     }
 
@@ -67,8 +68,8 @@ public class TestMathLib {
     @Test
     public void nSqrtTest() {
         assertEquals(6,mathLib.nSqrt(216,3),0);
-        assertEquals(1.005598786218176,mathLib.nSqrt(67.137,753.467),0);
-        assertEquals(0.975707169815815,mathLib.nSqrt(4,-56.37),0);
+        assertEquals(1.005598786218176,mathLib.nSqrt(67.137,753.467),delta);
+        assertEquals(0.975707169815815,mathLib.nSqrt(4,-56.37),delta);
     }
 
     @Test(expected = ArithmeticException.class)
@@ -79,15 +80,14 @@ public class TestMathLib {
     @Test
     public void powTest() {
         assertEquals(43681,mathLib.pow(209),0);
-        assertEquals(581172.49100089,mathLib.pow(762.3467),0);
-        assertEquals(29.56781675432209,mathLib.pow(5.4376297),0);
-        assertEquals(4069.39374724, mathLib.pow(-63.7918),0);
+        assertEquals(581172.49100089,mathLib.pow(762.3467),delta);
+        assertEquals(29.5677112644,mathLib.pow(5.43762),delta);
     }
 
     @Test
     public void nPowTest() {
         assertEquals(4096,mathLib.nPow(8,4),0);
-        assertEquals(1005102.747883873097743,mathLib.nPow(24.3687,4.328),0);
+        assertEquals(1005102.747883873097743,mathLib.nPow(24.3687,4.328),delta);
     }
 
     @Test
@@ -102,18 +102,20 @@ public class TestMathLib {
 
     @Test
     public void tanTest() {
-        assertEquals(1.60033452904105,mathLib.tan(58),0);
-        assertEquals(4.142994851093586,mathLib.tan(-643.57),0);
+        assertEquals(1.60033452904105,mathLib.tan(58),delta);
+        assertEquals(4.142994851093586,mathLib.tan(-643.57),delta);
     }
 
     @Test
     public void cosTest() {
-        assertEquals(0.8660254038,mathLib.cos(30),0);
+        assertEquals(0.866025403784439,mathLib.cos(30),delta);
+        assertEquals(0.975029342307275,mathLib.cos(-1092.831),delta);
     }
 
     @Test
     public void sinTest() {
-        assertEquals(0.6427876097,mathLib.sin(40),0);
+        assertEquals(0.642787609686539,mathLib.sin(40),delta);
+        assertEquals(0.472443089813982,mathLib.sin(28.193),delta);
     }
 
 }
