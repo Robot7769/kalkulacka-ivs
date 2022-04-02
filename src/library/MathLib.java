f1f4c7cb9e7fc80e8122157aaa52121db59df8fa
Robot7769 <jan.robot7@gmail.com>
2022-04-02 19:44:31 +0200

    Popisy funkcí a oprava factoriálu

--- src/library/MathLib.java
+++ src/library/MathLib.java
@@ -1,23 +1,51 @@
 package library;
 
 public class MathLib {
-
+    /**
+     * Privátní funkce absolutní hodnoty
+     * @param a hodnota
+     * @return Vrací absolutní hodnotu čísla 'a'
+     */
     private double abs(double a) {
         return a < 0 ? -a : a;
     }
 
+    /**
+     * Funkce sčítá hodnoty 'a' a 'b'
+     * @param a sčítanec b
+     * @param b sčítanec b
+     * @return Vrací sočet 'a + b'
+     */
     public double plus(double a, double b) {
         return a + b;
     }
 
+    /**
+     * Funkce odečítá hodnoty 'b' od 'a'
+     * @param a  menšenec
+     * @param b  menšitel
+     * @return Vrací rodíl 'a - b'
+     */
     public double minus(double a, double b) {
         return a - b;
     }
 
+    /**
+     * Funkce násobí hodnoty 'a' a 'b'
+     * @param a  činitel
+     * @param b  činitel
+     * @return Vrací součin 'a * b'
+     */
     public double mul(double a, double b) {
         return a * b;
     }
 
+    /**
+     * Funkce dělí hodnotu 'a' hodnotou 'b'
+     * @param a  čitatel
+     * @param b  jmenovatel
+     * @return Vrací podíl 'a / b'
+     */
     public double div(double a, double b) {
         if (b == 0) {
             throw new ArithmeticException("Nelze dělit nulou");
@@ -25,14 +53,31 @@ public class MathLib {
         return a / b;
     }
 
+    /**
+     * Funkce 'a' modulo 'b'
+     * @param a  čitatel
+     * @param b  jmenovatel
+     * @return Vrací zbytek po dělení  'a / b'
+     */
     public double mod(double a, double b) {
         return a % b;
     }
 
+    /**
+     * Funkce na výpočet druhé odmocniny z čísla 'a'
+     * @param a  čitatel
+     * @return Vrací '√a'
+     */
     public double sqrt(double a) {
         return nSqrt(a,2);
     }
 
+    /**
+     * Funkce na výpočet n-té odmocnině z čísla 'a'
+     * @param a odmocněnec
+     * @param n odmocnitel
+     * @return Vrací 'ᶰ√a'
+     */
     public double nSqrt(double a, int n) {
         if (n == 0) {
             throw new ArithmeticException("Není definována nultá odmocnina");
@@ -66,16 +111,20 @@ public class MathLib {
         }
     }
 
+    /**
+     * Funkce na výpočet druhé mocniny z čísla 'a'
+     * @param a  čitatel
+     * @return Vrací 'aᴺ'
+     */
     public double pow(double a) {
         return a * a;
     }
 
     /**
      * Funkce na výpočet n-té mocniny čísla 'a'
-     *
-     * @param a zadané číslo
-     * @param n musí být větší jak 1 (n > 1)
-     * @return  a^n
+     * @param a základ
+     * @param n exponent
+     * @return  'aᴺ'
      */
     public double nPow(double a, int n) {
         double a_n = 1;
@@ -89,26 +138,23 @@ public class MathLib {
         }
 
     }
-    private double calFact(int a) {
-        if (a == 1) {
-            return 1;
-        }
-        return a * (calFact(a - 1));
-    }
 
+    /**
+     * Funkce na výpočet n-té mocniny čísla 'a'
+     * @param a základ
+     * @return  'aᴺ'
+     */
     public double fact(int a) {
         if (a > 170) {
             throw new ArithmeticException("Příliš velký faktoriál čísla " + a);
         }
-        int sing = 1;
         if (a < 0) {
-            sing = -1;
-            a = (int) abs(a);
+            throw new ArithmeticException("Factorial ze záporného čísla není definovám");
         }
         if (a == 1 || a == 0) {
-            return sing;
+            return 1;
         }
-        return sing * calFact(a);
+        return a * (fact(a - 1));
     }
 
     public double pi() {
@@ -145,5 +191,5 @@ public class MathLib {
         }
         return sign*sina;
     }
-
+    //TODO vytvořit funkci math.log10  - logaritmus 10
 }
