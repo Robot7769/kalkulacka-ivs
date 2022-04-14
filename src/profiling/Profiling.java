4abe0fbd3d20c6a9d5c6a85e663290a004271bc6
p-cafourek <cafikuv2@gmail.com>
2022-04-14 03:03:58 +0200

    Profiling_data_read_fix_0.3 + data.txt for testing

--- src/profiling/Profiling.java
+++ src/profiling/Profiling.java
@@ -2,26 +2,26 @@ package profiling;
 
 import main.java.library.MathLib; //Matematická knihovna
 
+import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
-import java.io.File;
 
 public class Profiling {
 
     private final main.java.library.MathLib MathLib = new MathLib(); //Objekt matematické knihovny
 
     public static void main(String[] args){
-        try {//Pokus o nacaitani
-            File Objective = new File("data.txt");
+        try {                                           //Nacitani            //TODO predelat aby se dalo nacitat pomoci java Profiling *.txt && presunout data do pole
+            File Objective = new File("src/profiling/data.txt");
             Scanner myReader = new Scanner(Objective);
-            while (myReader.hasNextLine()){
-                String data = myReader.nextLine();
+            while (myReader.hasNext()){
+                String data = myReader.next();
                 System.out.println(data);
             }
             myReader.close();
-        } catch (FileNotFoundException error) {
+        } catch (FileNotFoundException e) {
             System.out.println("Soubor nebyl nalezen.");
-            //error.printStackTrace();
+            //e.printStackTrace();
         }
 
 
