beefa16c7bb0e075aea1d29f9773860494bd3e1b
p-cafourek <cafikuv2@gmail.com>
2022-04-16 23:48:21 +0200

    Vypocet profilingu

--- src/profiling/src/main/java/fit/ivs/calc/profiling/Profiling.java
+++ src/profiling/src/main/java/fit/ivs/calc/profiling/Profiling.java
@@ -10,16 +10,9 @@ public class Profiling {
     private static final MathLib mathLib = new MathLib(); //Objekt matematické knihovny
 
     public static void main(String[] args) {
-        System.out.println(" -- Test -- ");
-        double result = mathLib.div(4, 2);
-        System.out.println("Result: " + result);
-
-        if(true) {
-            return;
-        }
         ArrayList<Double> Values = new ArrayList<>();
         //Nacitani            //TODO predelat aby se dalo nacitat pomoci java Profiling *.txt
-        File Objective = new File("src/profiling/data.txt");
+        File Objective = new File("data.txt");
         if (!Objective.exists() || !Objective.canRead()) {
             System.out.println("Soubor nebyl nalezen.");
         }
@@ -46,25 +39,28 @@ public class Profiling {
 //     for (int j = 0; j < Values.size(); j++) { //test vypisu dat
         //          System.out.println(Values.get(j));
         //    }
-/*
+
         double counter = 0.0;
+        double counterPower = 0.0;
         double NumOfNum = Values.size();
 
         for (int i = 0; i < Values.size(); i++) {
-            counter += Values.get(i);
+            counter = mathLib.plus(counter, Values.get(i));
+            counterPower = mathLib.plus(counterPower, mathLib.pow(Values.get(i)));
         }
 
+        double scale = mathLib.div(counter, NumOfNum);
 
-        double scale = MathLib.div(counter, NumOfNum);
+        double numerator = mathLib.minus(counterPower, mathLib.mul(NumOfNum,mathLib.pow(scale)));
+        double denominator = mathLib.minus(NumOfNum,1);
 
-        System.out.println(MathLib.pow(counter));
-        System.out.println(MathLib.mul(Values.size(), MathLib.pow(scale)));
+        //System.out.println(mathLib.pow(counter));
+        //System.out.println(mathLib.mul(Values.size(), mathLib.pow(scale)));
 
-        double root = MathLib.mul(MathLib.div(1,MathLib.minus(1,Values.size())),MathLib.minus(MathLib.pow(counter) ,MathLib.mul(Values.size(), MathLib.pow(scale))));
+        double root = mathLib.div(numerator,denominator);
+        double amplitude = mathLib.sqrt(root);
 
-        double amplitude = MathLib.sqrt(root);
-
-        System.out.println("\n"+ amplitude);
-    */
+        System.out.println(" -- Test -- ");
+        System.out.println("Result: "+ amplitude);
     }
 }
