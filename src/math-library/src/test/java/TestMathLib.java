60fb2a0df9674e59480ed5faaff4b1b17ce1df80
Robot7769 <jan.robot7@gmail.com>
2022-04-29 02:27:12 +0200

    Poznámky k testům

--- src/math-library/src/test/java/TestMathLib.java
+++ src/math-library/src/test/java/TestMathLib.java
@@ -1,3 +1,23 @@
+/****************************************************
+ * Název projektu: Kalkulačka
+ * Soubor: TestMathLib.java
+ * Autoři: Vítězslav Šafář (xsafar26)
+ *         Jan Škrabal (xskrab12)
+ *         Richard Kocián (xkocia19)
+ *         Petr Cafourek (xcafou01)
+ *
+ * Popis: Testy nad matematickou knihovnou
+ ****************************************************/
+/**
+ * @file TestMathLib.java
+ *
+ * @brief Testy nad matematickou knihovnou
+ * @author Vítězslav Šafář (xsafar26)
+ * @author Jan Škrabal (xskrab12)
+ * @author Richard Kocián (xkocia19)
+ * @author Petr Cafourek (xcafou01)
+ */
+
 import org.junit.*;
 import fit.ivs.calc.mathlibrary.MathLib;
 import static org.junit.Assert.assertEquals;
@@ -6,13 +26,16 @@ import static org.junit.Assert.assertNotEquals;
 public class TestMathLib {
 
     private MathLib mathLib;
-    private final double delta = 0.0000000000001;
+    private final double delta = 0.0000000000001;  //tolerovaná přesnost
 
     @Before
     public void setUp() {
         mathLib = new MathLib();
     }
 
+    /**
+     * Testy nad funkcí plus
+     */
     @Test
     public void plusTest() {
         assertEquals(60,mathLib.plus(30,30),0);
@@ -22,6 +45,9 @@ public class TestMathLib {
         assertEquals(421.5865,mathLib.plus(-46.5481,468.1346),0);
     }
 
+    /**
+     * Testy nad funkcí mínus
+     */
     @Test
     public void minusTest() {
         assertEquals(0,mathLib.minus(30,30),0);
@@ -31,6 +57,9 @@ public class TestMathLib {
         assertEquals(6745.2455885459,mathLib.minus(3323.8223442329,-3421.423244313),0);
     }
 
+    /**
+     * Testy nad funkcí násobení
+     */
     @Test
     public void mulTest() {
         assertEquals(15,mathLib.mul(3,5),0);
@@ -39,6 +68,9 @@ public class TestMathLib {
         assertEquals(35614426.3776875,mathLib.mul(4628.6375, 7694.365),delta);
     }
 
+    /**
+     * Testy nad funkcí dělení
+     */
     @Test
     public void divTest() {
         assertEquals(16,mathLib.div(64,4),0);
@@ -46,11 +78,17 @@ public class TestMathLib {
         assertEquals(4.114015916158117,mathLib.div(-264.73569,-64.3497),delta);
     }
 
+    /**
+     * Test chybového stavu při dělení
+     */
     @Test(expected = ArithmeticException.class)
     public void divExceptionTest() {
         mathLib.div(7.7,0);
     }
 
+    /**
+     * Testy nad funkcí modulo
+     */
     @Test
     public void modTest() {
         assertEquals(0,mathLib.mod(20,5),0);
@@ -58,6 +96,9 @@ public class TestMathLib {
         assertEquals(-23.54607939999994,mathLib.mod(-2329.294132,384.2913421),delta);
     }
 
+    /**
+     * Testy nad funkcí odmocnina
+     */
     @Test
     public void sqrtTest() {
         assertEquals(4,mathLib.sqrt(16),0);
@@ -73,21 +114,32 @@ public class TestMathLib {
         assertEquals(0.8017880019057406,mathLib.sqrt(0.642864),delta);
     }
 
+    /**
+     * Test chybového stavu odmocnině ze záporného čísla
+     */
     @Test(expected = ArithmeticException.class)
     public void sqrtExceptionTest1() {
         mathLib.sqrt(-1);
 
     }
+    /**
+     * Test chybového stavu odmocnině ze záporného desetiného čísla
+     */
     @Test(expected = ArithmeticException.class)
     public void sqrtExceptionTest2() {
         mathLib.sqrt(-0.7);
     }
+    /**
+     * Test chybového stavu odmocnině ze záporného desetiného čísla na více desetiných míst
+     */
     @Test(expected = ArithmeticException.class)
     public void sqrtExceptionTest3() {
         mathLib.sqrt(-0.859362472);
     }
 
-
+    /**
+     * Testy nad funkcí n-té odmocniny
+     */
     @Test
     public void nSqrtTest() {
         assertEquals(6,mathLib.nSqrt(216,3),0);
@@ -100,31 +152,49 @@ public class TestMathLib {
         assertEquals(-0.9404247266162327,mathLib.nSqrt(-0.45,13),delta);
     }
 
+    /**
+     * Test chybového stavu sudé omocniny ze záporného čísla
+     */
     @Test(expected = ArithmeticException.class)
     public void nSqrtExceptionTest() {
         mathLib.nSqrt(-1,4);
     }
 
+    /**
+     * Test chybového stavu n-té omocniny z nuly
+     */
     @Test(expected = ArithmeticException.class)
     public void nSqrtExceptionTest2() {
         mathLib.nSqrt(0,-20);
     }
 
+    /**
+     * Test chybového stavu nulté omocniny z čísla
+     */
     @Test(expected = ArithmeticException.class)
     public void nSqrtExceptionTest3() {
         mathLib.nSqrt(43,0);
     }
 
+    /**
+     * Test chybového stavu nulté omocniny z čísla
+     */
     @Test(expected = ArithmeticException.class)
     public void nSqrtExceptionTest4() {
         mathLib.nSqrt(78,0);
     }
 
+    /**
+     * Test chybového stavu sudé omocniny ze záporného desetiného čísla
+     */
     @Test(expected = ArithmeticException.class)
     public void nSqrtExceptionTest5() {
         mathLib.nSqrt(-0.47,8);
     }
 
+    /**
+     * Testy nad funkcí druhé mocniny
+     */
     @Test
     public void powTest() {
         assertEquals(43681,mathLib.pow(209),0);
@@ -137,6 +207,9 @@ public class TestMathLib {
         assertEquals(0.21064180322329,mathLib.pow(-0.4589573),delta);
     }
 
+    /**
+     * Testy nad funkcí n-té mocniny
+     */
     @Test
     public void nPowTest() {
         assertEquals(4096,mathLib.nPow(8,4),0);
@@ -151,6 +224,9 @@ public class TestMathLib {
         assertEquals(0.0000039923420738,mathLib.nPow(0.323,11),delta);
     }
 
+    /**
+     * Testy nad funkcí faktoriál
+     */
     @Test
     public void factTest() {
         assertEquals(362880,mathLib.fact(9),0);
@@ -159,34 +235,51 @@ public class TestMathLib {
         assertEquals(1,mathLib.fact(1),0);
     }
 
-
+    /**
+     * Test chybového stavu faktoriálu ze záporného čísla
+     */
     @Test(expected = ArithmeticException.class)
     public void factTestException() {
         mathLib.fact(-7);
     }
 
+    /**
+     * Test chybového stavu faktoriálu příliš velkého čísla
+     */
     @Test(expected = ArithmeticException.class)
     public void factTestException2() {
         mathLib.fact(171);
     }
 
+    /**
+     * Test nad funkcí pí
+     */
     @Test
     public void piTest() {
         assertEquals(3.141592653589793,mathLib.pi(),0);
     }
 
+    /**
+     * Testy nad funkcí tangensu
+     */
     @Test
     public void tanTest() {
         assertEquals(1.60033452904105,mathLib.tan(58),delta);
         assertEquals(4.142994851093586,mathLib.tan(-643.57),delta);
     }
 
+    /**
+     * Testy nad funkcí kosinu
+     */
     @Test
     public void cosTest() {
         assertEquals(0.866025403784439,mathLib.cos(30),delta);
         assertEquals(0.975029342307275,mathLib.cos(-1092.831),delta);
     }
 
+    /**
+     * Testy nad funkcí sinu
+     */
     @Test
     public void sinTest() {
         assertEquals(0.642787609686539,mathLib.sin(40),delta);
